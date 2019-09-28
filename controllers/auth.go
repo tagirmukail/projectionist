1cbcafd35ebe21fde786e90b758349b71791cf3f
taga <Tagir.Mukailov@regium.com>
2019-09-28 09:59:22 +0300

    fixed authentication

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
@@ -22,7 +23,7 @@ func LoginApi(
 		var form = forms.LoginForm{}
 		var err = json.NewDecoder(r.Body).Decode(&form)
 		if err != nil {
-			respond = utils.Message(false, "Invaid parameters")
+			respond = utils.Message(false, consts.InputDataInvalidResp)
 			resp.WriteHeader(http.StatusBadRequest)
 			utils.JsonRespond(resp, respond)
 			return
@@ -30,17 +31,35 @@ func LoginApi(
 
 		err = form.Validate()
 		if err != nil {
-			respond = utils.Message(false, err.Error())
+			respond = utils.Message(false, consts.InputDataInvalidResp)
 			resp.WriteHeader(http.StatusBadRequest)
 			utils.JsonRespond(resp, respond)
 			return
 		}
 
-		var user = models.User{}
+		var iUser models.Model
+		var user = &models.User{}
 
-		if err = dbProvider.GetByName(&user, form.Username); err != nil {
-			respond = utils.Message(false, consts.NotExistResp)
+		iUser, err = dbProvider.GetByName(user, form.Username)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				respond = utils.Message(false, consts.NotExistResp)
+				resp.WriteHeader(http.StatusNotFound)
+				utils.JsonRespond(resp, respond)
+				return
+			}
+
+			respond = utils.Message(false, consts.SmtWhenWrongResp)
 			log.Printf("LoginApi() error: %v", err)
+			resp.WriteHeader(http.StatusInternalServerError)
+			utils.JsonRespond(resp, respond)
+			return
+		}
+
+		user, ok := iUser.(*models.User)
+		if !ok {
+			respond = utils.Message(false, consts.SmtWhenWrongResp)
+			log.Printf("LoginApi() error: iUser is not User model")
 			resp.WriteHeader(http.StatusInternalServerError)
 			utils.JsonRespond(resp, respond)
 			return
@@ -68,7 +87,7 @@ func LoginApi(
 		respond = utils.Message(true, "Login successful")
 		user.Password = ""
 		user.Token = tokenStr
-		respond["userID"] = user
+		respond["user"] = user
 		utils.JsonRespond(resp, respond)
 
 	})
